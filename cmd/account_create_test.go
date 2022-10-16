cf091feb4098adfb9de80db9a112fe87c058bb02
loikg <mail@company.com>
2022-10-16 12:18:05 +0800

    tests: add account create test + update CI

---
+++ cmd/account_create_test.go
@@ -0,0 +1,29 @@
+package cmd_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/loikg/hedera-cli/cmd"
+	"github.com/loikg/hedera-cli/internal"
+	"github.com/loikg/hedera-cli/pkg/hederatest"
+)
+
+func TestAccountCreateCommand(t *testing.T) {
+	actual := new(bytes.Buffer)
+	cmd.RootCmd.SetOut(actual)
+	cmd.RootCmd.SetErr(actual)
+
+	cmd.RootCmd.SetArgs([]string{"account", "create", "--balance", "10.5"})
+	cmd.RootCmd.Execute()
+
+	var data internal.M
+	err := json.Unmarshal(actual.Bytes(), &data)
+	if err != nil {
+		t.Fatalf("failed to unmarshal command output: %v", err)
+	}
+
+	hederatest.AssertValidAccountID(t, data["accountId"])
+	hederatest.AssertValidKeyPair(t, data["privateKey"], data["publicKey"])
+}
---
+++ pkg/hederatest/hederatest.go
@@ -0,0 +1,28 @@
+package hederatest
+
+import (
+	"testing"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func AssertValidAccountID(t *testing.T, accountID interface{}) {
+	t.Helper()
+	assert.IsType(t, "string", accountID)
+	_, err := hedera.AccountIDFromString(accountID.(string))
+	if err != nil {
+		t.Errorf("expected %s to be a valid account id: %v", accountID, err)
+	}
+}
+
+func AssertValidKeyPair(t *testing.T, privateKey interface{}, publicKey interface{}) {
+	t.Helper()
+	assert.IsType(t, "string", privateKey)
+	assert.IsType(t, "string", publicKey)
+	key, err := hedera.PrivateKeyFromStringEd25519(privateKey.(string))
+	if err != nil {
+		t.Fatalf("failed parse private key %s: %v", privateKey, err)
+	}
+	assert.Equal(t, key.PublicKey().StringRaw(), publicKey)
+}
